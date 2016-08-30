cb9ecdc03945deae5f50314efa51f64755803204
thomas <dui1cuo@126.com>
2016-08-30 15:40:39 +0800

    [Mod]add search view controller

---
+++ SearchControllerDemo/SearchControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchControllerDemo
+//
+//  Created by admin on 16/8/30.
+//  Copyright © 2016年 thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearchControllerDemo/SearchControllerDemo/SearchViewController.m
@@ -0,0 +1,28 @@
+//
+//  SearchViewController.m
+//  SearchControllerDemo
+//
+//  Created by admin on 16/8/30.
+//  Copyright © 2016年 thomas. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@interface SearchViewController ()
+
+@end
+
+@implementation SearchViewController
+
+#pragma mark - life cycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
